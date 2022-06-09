164c06bb659b1fa013f0f91e5bbcb44c87a757f4
Heike Kasper <heike.kasper@kadelo.de>
2022-06-09 10:15:49 +0200

    added main structure html

---
+++ script.js
@@ -0,0 +1,9 @@
+//Building Teams 4 inputs; Team 1 and Team2
+//Random Team Button
+//victory Points (5,10,king,dragon,phoenix) cards as buttons onclick:
+//gray and not clickable, add value to points of the Team
+//next round if nobody has reached 1000 points 
+//round counter and bomb counter
+//reset button to start a new game
+
+//statistic of the same Teams
