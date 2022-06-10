c66252e35128e2203bf07f3c3cde2f187fe5d4f2
Heike Kasper <heike.kasper@kadelo.de>
2022-06-10 10:42:13 +0200

    added function for card values

--- script.js
+++ script.js
@@ -12,12 +12,21 @@ let team2 = 0;
 let activeButtonTeam1;
 let activeButtonTeam2;
 
+const buttonPoints = document.getElementsByClassName("card");
+for(let i = 0; i < buttonPoints.length; i++){
+    buttonPoints[i].addEventListener("click", function(event){
+    const pointValue = event.target.value;
+    const pointValueNumber = parseInt(pointValue);
+console.log(pointValueNumber);
+})
+    }
+
 function scoringTeam1(){
     const buttonTeam1 = document.getElementById("team1");
      buttonTeam1.addEventListener('click', function(){
         activeButtonTeam1 = true;
         activeButtonTeam2 = false;
-        console.log(activeButtonTeam1,activeButtonTeam2 )
+        console.log(activeButtonTeam1,activeButtonTeam2)
      })
 }
 function scoringTeam2(){
@@ -28,3 +37,6 @@ function scoringTeam2(){
         console.log(activeButtonTeam1,activeButtonTeam2 )
      })
 }
+
+
+
