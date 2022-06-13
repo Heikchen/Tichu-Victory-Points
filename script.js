a8abe996964193b831cf7fea36adc6d6a4fca61d
Heike Kasper <heike.kasper@kadelo.de>
2022-06-13 14:25:08 +0200

    change value in case of double click

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let activeButtonTeam1;
 let activeButtonTeam2;
 
 
+//choice which team is active
 const buttonClick = document.getElementsByClassName("team-button");
 for(let i = 0; i < buttonClick.length; i++){
     buttonClick[i].addEventListener("click", function(event){  
@@ -24,27 +25,44 @@ for(let i = 0; i < buttonClick.length; i++){
                 activeButtonTeam1 = false;
                 activeButtonTeam2 = true;}
            console.log(activeButtonTeam1, activeButtonTeam2);
-           
-        })
-}
-
+           })}
+//add Points from cards to team
 const buttonPoints = document.getElementsByClassName("card");
     for(let i = 0; i < buttonPoints.length; i++){
         buttonPoints[i].addEventListener("click", function(event){
         let pointValue = event.target.value;
         let pointValueNumber = parseInt(pointValue);
+        
 if (activeButtonTeam1 === true){
     team1 = team1 + pointValueNumber;
-    console.log(team1, team2); 
+    console.log(team1, team2);
+    var buttonCardsId = event.target.id;
+    let buttonCards = document.getElementById(buttonCardsId);
+    buttonCards.classList.toggle("button-click");
     }
-else { team2 = team2 + pointValueNumber
-        console.log(team1, team2);  } 
+else if (activeButtonTeam2 === true){ 
+    team2 = team2 + pointValueNumber;
+        console.log(team1, team2); 
+        var buttonCardsId = event.target.id;
+        let buttonCards = document.getElementById(buttonCardsId);
+        buttonCards.classList.toggle("button-click");} 
     })
     }
+    //change Value in case of double click if a Team is selected
+    const changeValueCards = document.getElementsByClassName("card");
+    for(let i = 0; i < changeValueCards.length; i++){
+        changeValueCards[i].addEventListener("click", function(event){ 
+            if(activeButtonTeam1 === true ||activeButtonTeam2 === true ){
+            let changeValue = event.target.value;
+            let changeValueNumber = parseInt(changeValue);
+            changeValueNumber = changeValueNumber * -1;
+            let changeValueString = changeValueNumber.toString();
+            changeValueCards[i].value=changeValueString;
+        console.log(changeValueCards[i])}
+        })
+       ;}
 
-
-
-
+   
 
 
     
